b299acd61939de8b47fed207c6ec0779953171d6
ThatLoki <komantouil@gmail.com>
2024-01-03 18:42:26 +0000

    update ~~

--- FileSharing/client.py
+++ FileSharing/client.py
@@ -8,15 +8,21 @@ class client:
         self.sock.connect((self.host, self.port))
 
     def send_data(self, data):
-        
         self.sock.sendall(data.encode())
 
     def receive_data(self):
         received_data = self.sock.recv(1024).decode()
         return received_data
 
-    def close_connection(self,data):
-        
+    def receive_file(self, filename):
+        with open(filename, 'wb') as file:
+            while True:
+                data = self.sock.recv(1024)
+                if not data:
+                    break
+                file.write(data)
+
+    def close_connection(self, data):
         self.sock.sendall(data.encode())
         self.sock.close()
 
@@ -25,4 +31,5 @@ if __name__ == "__main__":
     client = client('192.168.1.39', 8000)
     client.send_data("Hello from client")
     print(client.receive_data())
+    client.receive_file("received_file.txt")
     client.close_connection("exit")
--- FileSharing/server.py
+++ FileSharing/server.py
@@ -1,14 +1,14 @@
 import os 
 import socket
 import threading
-import time 
 
-class server :
-    def __init__(self,host,port):
+
+class server:
+    def __init__(self, host, port):
         self.host = host 
         self.port = port 
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sock.bind((self.host,self.port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.host, self.port))
 
     
     def start(self):
@@ -25,8 +25,22 @@ class server :
             message = client.recv(1024).decode()
             if message:
                 print(f"Received message from client: {message}")
+                if message == "send_file":
+                    self.send_file(client, "path/to/file.txt")  # Replace with the actual file path
             else:
                 break
+
+    def send_file(self, client, file_path):
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as file:
+                data = file.read(1024)
+                while data:
+                    client.send(data)
+                    data = file.read(1024)
+            print("File sent successfully")
+        else:
+            print("File not found")
+
 
 
 if __name__ == "__main__":
