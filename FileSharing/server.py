717030fd78987e997e90e28d431657e2e3c9bf2a
ThatLoki <komantouil@gmail.com>
2024-01-05 14:59:02 +0000

    another update

--- FileSharing/server.py
+++ FileSharing/server.py
@@ -2,21 +2,20 @@ import os
 import socket
 import threading
 
-
 class server:
     def __init__(self, host, port):
         self.host = host 
         self.port = port 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.host, self.port))
+        self.running = False
 
-    
     def start(self):
         self.sock.listen(5)
-        print(f"Server listening on {self.host}:{self.port}")
-        while True:
-            client, address = self.sock.accept()
-            print(f"New connection from {address[0]}:{address[1]}")
+        self.running = True
+        while self.running:
+            client, adress = self.sock.accept() 
+            print(f"Client connected {adress[0]} port : {adress[1]}")
             client_thread = threading.Thread(target=self.handle_client, args=(client,))
             client_thread.start()
 
@@ -25,8 +24,14 @@ class server:
             message = client.recv(1024).decode()
             if message:
                 print(f"Received message from client: {message}")
-                if message == "send_file":
-                    self.send_file(client, "C:/Users/Administrator/Documents/Projects/FileSharing/FileSharing/client_gui.txt")  # Replace with the actual file path
+                if message == "send":
+                    file_path = client.recv(1024).decode()
+                    self.send_file(client, file_path)
+                elif message == "receive_file":
+                    file_path = client.recv(1024).decode()
+                    self.receive_file(client, file_path)
+                else:
+                    client.send(message.encode())
             else:
                 break
 
@@ -40,7 +45,18 @@ class server:
             print("File sent successfully")
         else:
             print("File not found")
+    
+    def receive_data(self):
+        received_data = self.sock.recv(1024).decode()
+        return received_data
+    
+    def send_data(self, data):
+        self.sock.send(data.encode())
 
+    def stop(self):
+        self.running = False
+        self.sock.close()
+    
 
 
 
--- FileSharing/server_gui.py
+++ FileSharing/server_gui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QThread
 from server import server
 import sys
@@ -16,20 +16,54 @@ class ServerGUI(QWidget):
         super().__init__()
         self.initUI()
 
+
     def initUI(self):
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Server GUI')
+        self.setWindowTitle('Server')
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
+
+        self.data_widget = QTextEdit()
+        self.data_widget.setReadOnly(True)
+        self.layout.addWidget(self.data_widget)
+
+        self.input_line = QLineEdit()
+        self.layout.addWidget(self.input_line)
+
 
         self.start_button = QPushButton('Start Server')
         self.start_button.clicked.connect(self.start_server)
         self.layout.addWidget(self.start_button)
 
+        self.stop_button = QPushButton('Stop Server')
+        self.stop_button.clicked.connect(self.stop_server)
+        self.layout.addWidget(self.stop_button)
+
+        self.send_button = QPushButton('Send')
+        self.send_button.clicked.connect(self.send_data)
+        self.layout.addWidget(self.send_button)
+
     def start_server(self):
         self.server_thread = ServerThread()
         self.server_thread.start()
+        if self.server_thread.isRunning():
+            self.data_widget.append(f"Server is running on adress {self.server_thread.server.host}:{self.server_thread.server.port}")
+        else:
+            self.data_widget.append("Server is not running")
+
+    def stop_server(self):
+        self.server_thread.server.stop()
+
+    def send_data(self):
+         data = self.input_line.text()
+         self.client_thread.server.send_data(data)
+         self.input_line.clear()
+
+    
+    def update_data(self, data):
+        server.receive_data(data)
+        self.data_widget.append(data)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
